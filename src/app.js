b98879e33ef8e1854d0cb6604a7886bf41798fe4
Dani-Bcn <nneodani@gmail.com>
2024-05-30 04:01:10 +0200

    update CORS link production

--- src/app.js
+++ src/app.js
@@ -11,8 +11,16 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://mis-historias-front-end.vercel.app/",
-    credentials:true
+  origin:(origin, () =>{
+    const ACCEPTED_ORIGINs=[
+      "http://localhost:5137",
+      "http://localhost:4000",
+      "http://localhost:8000",
+      "https://mis-historias-front-end.vercel.app/",
+      "https://mis-historias-back-end.vercel.app/"
+    ]
+  })
+  
   }));
 app.use(authRouter)
 app.use(uploadImg)
