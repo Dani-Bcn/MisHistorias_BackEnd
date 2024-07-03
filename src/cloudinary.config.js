78542a99ee64bf0ff1602c7fb9390329fc753335
Dani-Bcn <nneodani@gmail.com>
2024-07-03 19:41:50 +0200

    test  update for vecel

--- src/cloudinary.config.js
+++ src/cloudinary.config.js
@@ -1,8 +1,10 @@
-/* import {v2 as cloudinary} from 'cloudinary';
+ import {v2 as cloudinary} from 'cloudinary';
 import  { CloudinaryStorage } from "multer-storage-cloudinary"
 import multer from 'multer';
 import dotenv from 'dotenv'
+
 dotenv.config()
+
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_NAME, 
   api_key: process.env.CLOUDINARY_KEY, 
@@ -10,15 +12,13 @@ cloudinary.config({
   api_url: process.env.CLOUDINARY_URL
 });
 
-const storage = new CloudinaryStorage({
-  
+const storage = new CloudinaryStorage({  
   cloudinary,
   params: {
     allowed_formats: ["jpg", "png","jpeg"],
     folder: "imgUsers" // The name of the folder in cloudinary
     // resource_type: "raw", // => this is in case you want to upload other types of files, not just images
-  }
- 
+  } 
 });
 
-export default multer({storage}) */
+export default multer({storage}) 
