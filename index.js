3a11a253939bc4a6dfc8f5783ecc7409d09e8583
Dani-Bcn <nneodani@gmail.com>
2024-08-10 11:28:22 +0200

    UpDate for Vercel

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ import {app} from "./src/app.js"
 import { connectDb } from "./src/db.js"  
 import "dotenv/config.js";
 
-
-
 const port = process.env.PORT || 8000;
- app.listen(port,()=>{
-    
-   console.log(`port runing in ${port}`)
-    
+
+ app.listen(port,()=>{    
+   console.log(`port runing in ${port}`)    
  }) 
-
-
+ 
 connectDb()
  
--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ export const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-  origin:"https://mis-historias-front-end-seven.vercel.app",
+  origin:"http://localhost:5173",
   credentials:true
 }));
 app.use(authRouter)
