af7f918c11e926f10ed18f7cf4487ce697c30ede
Dani-Bcn <nneodani@gmail.com>
2024-08-15 02:40:31 +0200

    UpDate for Vercel

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,17 +9,17 @@ const router = Router();
 router.put("/api/removeBookLibrary/:userId/:bookId", removeBookLibrary);
 router.post("/api/registerUser", registerUser);
 router.post("/api/logoutUser",logoutUser);
-router.put("/api/editUser/:id", editUser);
-router.get("/api/profile", profile);
+router.put("/api/editUser/:id", authRequired,editUser);
+router.get("/api/profile", authRequired,profile);
 router.post("/api/loginUser",  loginUser);
-router.post("/api/createBook", createBook);
+router.post("/api/createBook", authRequired,createBook);
 router.get("/api/getAllBooks", getAllBooks);
 router.get("/api/getAllUsers", getAllUsers);
 router.get("/api/getBook/:id", getBook);
-router.delete("/api/deleteBook/:id", deleteBook);
-router.put("/api/editBook/:id", editBook);
-router.get("/api/booksUser", booksUser);
-router.put("/api/addBook/:userId/:bookId", addBook);
+router.delete("/api/deleteBook/:id", authRequired,deleteBook);
+router.put("/api/editBook/:id", authRequired,editBook);
+router.get("/api/booksUser", authRequired,booksUser);
+router.put("/api/addBook/:userId/:bookId", authRequired, addBook);
 router.post("/api/savePage",  savePages);
 router.post("/api/deleteImg", deleteImage)
   
