70e15ed7646414e463282e5fd4d157f0d49be860
Dani-Bcn <nneodani@gmail.com>
2024-08-17 05:30:04 +0200

    UpDate for Vercel

--- src/app.js
+++ src/app.js
@@ -10,12 +10,17 @@ export const app = express()
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({
-  origin:"https://mis-historias-front-end-seven.vercel.app",
+const corsOptions = {
+  origin: 'http://localhost:8000',
   credentials:true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',  
-}));
+  allowedHeaders: 'Content-Type,Authorization',
+}
+app.use(cors(corsOptions))
+app.use((req, res, next) => {
+  req.header('Access-Control-Allow-Credentials', true);
+  next();
+});
  app.use(authRouter) 
 app.use(uploadImg)
 app.use(deleteImg)
