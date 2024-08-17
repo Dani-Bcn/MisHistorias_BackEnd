d17d057d3d86ddbe6c6a7c393f66abac1ce28a35
Dani-Bcn <nneodani@gmail.com>
2024-08-17 04:13:16 +0200

    UpDate for Vercel

--- src/app.js
+++ src/app.js
@@ -1,33 +1,26 @@
-import express from "express"
-import authRouter from "./routes/routes.js"
-import uploadImg from "./controllers/userControllers.js"
-import deleteImg from "./controllers/userControllers.js"
-import morgan from "morgan"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import authRouter from "./routes/routes.js";
+import uploadImg from "./controllers/userControllers.js";
+import deleteImg from "./controllers/userControllers.js";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-export const app = express()
+export const app = express();
 
-app.use(cookieParser())
-app.use(express.json())
-app.use(cors({
-  origin:"https://mis-historias-front-end-seven.vercel.app",
-
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',  
-  cookie : {
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://mis-historias-front-end-seven.vercel.app",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
     secure: true,
-    sameSite: 'None'
-  }
-}));
- app.use(authRouter) 
-app.use(uploadImg)
-app.use(deleteImg)
-app.use(morgan("dev"))
-
-
-
-
-
-
-
+    sameSite: "None",
+  })
+);
+app.use(authRouter);
+app.use(uploadImg);
+app.use(deleteImg);
+app.use(morgan("dev"));
