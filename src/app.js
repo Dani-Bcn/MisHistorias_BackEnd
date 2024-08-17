e8c7e7c07fe188d7d0b138924365f1e487cad336
Dani-Bcn <nneodani@gmail.com>
2024-08-17 05:37:00 +0200

    UpDate for Vercel

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
   req.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
  app.use(authRouter) 
