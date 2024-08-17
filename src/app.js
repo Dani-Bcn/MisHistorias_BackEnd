37ae16bf4a525b378b17ed660df800e09d9921d5
Dani-Bcn <nneodani@gmail.com>
2024-08-17 04:11:50 +0200

    UpDate for Vercel

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
   origin:"https://mis-historias-front-end-seven.vercel.app",
-  credentials:true,
+
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',  
   cookie : {
