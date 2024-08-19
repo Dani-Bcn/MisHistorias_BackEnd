34676cd4f80bafe9f7111a83a2e0c11c26e991e1
Dani-Bcn <nneodani@gmail.com>
2024-08-19 10:00:38 +0200

    UpDate for Vercel

--- src/midlewares/validateText.js
+++ src/midlewares/validateText.js
@@ -1,4 +1,4 @@
-export const validateSchema = (schema) => (req, res, next) => {
+/* export const validateSchema = (schema) => (req, res, next) => {
   try {
     schema.parse(req.body);
     next();
@@ -8,3 +8,4 @@ export const validateSchema = (schema) => (req, res, next) => {
     })
   }
 };
+ */
--- src/midlewares/validateToken.js
+++ src/midlewares/validateToken.js
@@ -2,14 +2,15 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = async (req, res, next) => {
-  res.cookie('token', token, /* {
+  const { token } = req.cookies;
+  res.cookie('token', token, {
     httpOnly: true, // la cookie no será accesible desde JavaScript en el navegador
     secure: true,   // asegura que la cookie solo se enviará a través de HTTPS
     sameSite: 'None', // necesario para permitir el uso de cookies cross-site
-    domain: 'tu-dominio.com', // dominio donde la cookie será accesible
+    domain: 'https://mis-historias-front-end-seven.vercel.app', // dominio donde la cookie será accesible
     path: '/',  // ruta donde la cookie será accesible
     expires: new Date(Date.now() + 8 * 3600000), // opcional, establece la expiración de la cookie
-  } */);
+  });
 
   if (!token) {
     res.json({ message: "no autorizado" });
