5fe85ebe64d853ad1ee0324f1291ef86c017b726
Dani-Bcn <nneodani@gmail.com>
2024-08-19 09:27:08 +0200

    UpDate for Vercel

--- src/midlewares/validateToken.js
+++ src/midlewares/validateToken.js
@@ -3,14 +3,7 @@ import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = async (req, res, next) => {
   const { token } = 
-  res.cookies({
-    httpOnly: true, // la cookie no será accesible desde JavaScript en el navegador
-    secure: true,   // asegura que la cookie solo se enviará a través de HTTPS
-    sameSite: 'None', // necesario para permitir el uso de cookies cross-site
-    domain: 'https://mis-historias-front-end-seven.vercel.app', // dominio donde la cookie será accesible
-    path: '/',  // ruta donde la cookie será accesible
-    expires: new Date(Date.now() + 8 * 3600000), // opcional, establece la expiración de la cookie
-  });
+  res.cookies;
 
   if (!token) {
     res.json({ message: "no autorizado" });
