d219dffcefee247eafb7c0b8d15c44087304baca
Dani-Bcn <nneodani@gmail.com>
2024-08-20 01:47:59 +0200

    UpDate for Vercel

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -112,6 +112,10 @@ export const getAllUsers = async (req, res) => {
 export const logoutUser = async (req, res) => {
  
   res.clearCookie("token");
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 5 * 1000),
+    httpOnly: true,
+})
   res.json({ message: "Sesion cerrada" });
 };
 
