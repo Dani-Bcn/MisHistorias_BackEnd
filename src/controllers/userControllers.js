bcd8cc06cbe841af25011db4f1eb7fea0c48c55c
Dani-Bcn <nneodani@gmail.com>
2024-08-20 01:50:43 +0200

    UpDate for Vercel

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -111,7 +111,7 @@ export const getAllUsers = async (req, res) => {
 
 export const logoutUser = async (req, res) => {
  
-  res.clearCookie("token");
+
   res.cookie('token', 'none', {
     expires: new Date(Date.now() + 5 * 1000),
     httpOnly: true,
