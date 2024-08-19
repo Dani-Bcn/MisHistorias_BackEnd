d59a4df05af3a0510e9918d3e0c40aa64f5d4b2a
Dani-Bcn <nneodani@gmail.com>
2024-08-19 09:29:37 +0200

    UpDate for Vercel

--- src/midlewares/validateToken.js
+++ src/midlewares/validateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = async (req, res, next) => {
-  const { token } = req.cookies;
+  const { token } = res.cookies;
 
   if (!token) {
     res.json({ message: "no autorizado" });
