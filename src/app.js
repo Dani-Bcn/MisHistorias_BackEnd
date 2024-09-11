2baef159713b0765bda45740cdc51b58a6e35494
Dani-Bcn <nneodani@gmail.com>
2024-09-11 16:04:08 +0200

    UpDate for Vercel

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ export const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-  origin: "https://mis-historias-front-end-seven.vercel.app/", // Dominio permitido
+  origin: "https://mis-historias-front-end-seven.vercel.app", // Dominio permitido
   credentials: true, // Necesario para que las cookies se envíen en solicitudes cross-site
   methods: ["GET", "POST", "PUT", "DELETE","delete"], // Métodos permitidos
   allowedHeaders: ["Content-Type", "Authorization"], // Headers permitidos
