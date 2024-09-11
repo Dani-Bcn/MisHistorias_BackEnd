07bc445cfe8e15c48ff8c6c13f744f405904abcc
Dani-Bcn <nneodani@gmail.com>
2024-09-11 15:13:27 +0200

    UpDate for Vercel

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -109,9 +109,9 @@ export const getAllUsers = async (req, res) => {
 export const logoutUser = async (req, res) => {
   res.cookie("token", "", {
     expires: new Date(0),
-    secure: true , // Debe ser true si estás usando sameSite: "none"
+   /*  secure: true , // Debe ser true si estás usando sameSite: "none"
       
-     sameSite: "none", // Necesario para permitir el uso de cookies cross-site
+     sameSite: "none", // Necesario para permitir el uso de cookies cross-site */
   });
   res.sendStatus(200);
 };
