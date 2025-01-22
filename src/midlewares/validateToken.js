c179be7dd8b4ab4c9cdd990da7b91dcb41767b78
Dani-Bcn <nneodani@gmail.com>
2025-01-22 21:48:35 +0100

    test profile

--- src/midlewares/validateToken.js
+++ src/midlewares/validateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = async (req, res, next) => {
-  const token = await req.cookies.token;
+  const token = await req.cookies;
   
  
 
